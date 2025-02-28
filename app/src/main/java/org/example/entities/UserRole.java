aa0f29435ea0321074834f4b2ef016f1aabca14b
pratik <pratikhanmante3@gmail.com>
2025-02-28 23:33:26 +0530

    Entities added

---
+++ app/src/main/java/org/example/entities/UserRole.java
@@ -0,0 +1,24 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table()
+public class UserRole {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column("role_id")
+    private Long roleID;
+
+    private String name;
+
+
+}
